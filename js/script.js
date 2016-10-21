54239f4cd262102d74e6385fa3a2f04094f61f9d
Cassie Peterson <cas.j.peters@gmail.com>
2016-10-21 14:15:30 -0600

    everything works. go team

--- js/script.js
+++ js/script.js
@@ -6,13 +6,22 @@ $(document).ready(function() {
 
 
 var subtotal = document.querySelector('#subtotal');
-var tax = document.querySelector('#tax')
-var total = document.querySelector('#total')
+var tax = document.querySelector('#tax');
+var total = document.querySelector('#total');
+
+subtotal.innerText = '$0.00';
+tax.innerText = '$0.00';
+total.innerText = '$0.00';
+// var pricePattern = /([0-9\.]+)/g;
+
+// var someRegEx = /[\w]/
 
 // EVENT LISTENER FOR BURGER
 var orderBurger = document.querySelector('#royaleCheese');
 
 orderBurger.addEventListener ('click', function () {
+  event.preventDefault();
+  // Add row with data to table
   var createTR = document.createElement('tr')
   var tdProduct = document.createElement('td');
   tdProduct.innerText = 'Royale with Cheese';
@@ -23,15 +32,51 @@ orderBurger.addEventListener ('click', function () {
   createTR.appendChild(tdPrice);
   var table = document.querySelector('#table');
   table.appendChild(createTR);
-  subtotal.innerText += 8.99;
-  tax.innerText += 0.63;
-  total.innerText += 9.62;
+
+  // Add price to subtotal
+  //pricePattern says get the digits and decimals for whatever this is applyed to. the g means global
+  var pricePattern = /([\d\.]+)/g;
+  //remove $ using the regEx
+  var matchesSubtotal = pricePattern.exec(subtotal.innerText);
+  var subtotalNum = parseFloat(matchesSubtotal[1]);
+  //add the shown price for one Royale to the current subtotal
+  subtotalNum += 8.99;
+  //make sure that it only returns two decimals
+  var fixedSubNum = subtotalNum.toFixed(2)
+  //add the fixedTaxNum to the tax inner text with a $
+  subtotal.innerText = ('$' + fixedSubNum);
+
+  //Add price to tax
+  //remove $
+  var sliceTax = tax.innerText.slice(1);
+  //make sliceTax a number
+  var taxNum = parseFloat(sliceTax);
+  //add the tax for one Royale to the current tax
+  taxNum += 0.26;
+  //make sure that it only returns two decimals
+  var fixedTaxNum = taxNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  tax.innerText = '$' + fixedTaxNum;
+
+  //Add price to total
+  //remove $
+  var sliceTotal = total.innerText.slice(1);
+  //make sliceTotal a number
+  var totalNum = parseFloat(sliceTotal);
+  //add the subtotal to the tax to the previous total
+  totalNum += 9.25;
+  //make sure that it only returns two decimals
+  var fixedTotalNum = totalNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  total.innerText = '$' + fixedTotalNum;
 });
 
 // EVENT LISTENER FOR PIE
 var orderPie = document.querySelector('#arugulaPie');
 
 orderPie.addEventListener ('click', function () {
+  event.preventDefault();
+  // Add row with data to table
   var createTR = document.createElement('tr')
   var tdProduct = document.createElement('td');
   tdProduct.innerText = 'Arugula Pie';
@@ -42,12 +87,51 @@ orderPie.addEventListener ('click', function () {
   createTR.appendChild(tdPrice);
   var table = document.querySelector('#table');
   table.appendChild(createTR);
+
+  // Add price to subtotal
+  //pricePattern says get the digits and decimals for whatever this is applyed to. the g means global
+  var pricePattern = /([\d\.]+)/g;
+  //remove $ using the regEx
+  var matchesSubtotal = pricePattern.exec(subtotal.innerText);
+  var subtotalNum = parseFloat(matchesSubtotal[1]);
+  //add the shown price for one Royale to the current subtotal
+  subtotalNum += 11.99;
+  //make sure that it only returns two decimals
+  var fixedSubNum = subtotalNum.toFixed(2)
+  //add the fixedTaxNum to the tax inner text with a $
+  subtotal.innerText = ('$' + fixedSubNum);
+
+  //Add price to tax
+  //remove $
+  var sliceTax = tax.innerText.slice(1);
+  //make sliceTax a number
+  var taxNum = parseFloat(sliceTax);
+  //add the tax for one Royale to the current tax
+  taxNum += 0.34;
+  //make sure that it only returns two decimals
+  var fixedTaxNum = taxNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  tax.innerText = '$' + fixedTaxNum;
+
+  //Add price to total
+  //remove $
+  var sliceTotal = total.innerText.slice(1);
+  //make sliceTotal a number
+  var totalNum = parseFloat(sliceTotal);
+  //add the subtotal to the tax to the previous total
+  totalNum += 12.33;
+  //make sure that it only returns two decimals
+  var fixedTotalNum = totalNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  total.innerText = '$' + fixedTotalNum;
 });
 
 // EVENT LISTENER FOR SMOKED SWINE
 var orderSwine = document.querySelector('#smokedSwine');
 
 orderSwine.addEventListener ('click', function () {
+  event.preventDefault();
+  // Add row with data to table
   var createTR = document.createElement('tr')
   var tdProduct = document.createElement('td');
   tdProduct.innerText = 'Smoked Swine';
@@ -58,12 +142,51 @@ orderSwine.addEventListener ('click', function () {
   createTR.appendChild(tdPrice);
   var table = document.querySelector('#table');
   table.appendChild(createTR);
+
+  // Add price to subtotal
+  //pricePattern says get the digits and decimals for whatever this is applyed to. the g means global
+  var pricePattern = /([\d\.]+)/g;
+  //remove $ using the regEx
+  var matchesSubtotal = pricePattern.exec(subtotal.innerText);
+  var subtotalNum = parseFloat(matchesSubtotal[1]);
+  //add the shown price for one Royale to the current subtotal
+  subtotalNum += 14.99;
+  //make sure that it only returns two decimals
+  var fixedSubNum = subtotalNum.toFixed(2)
+  //add the fixedTaxNum to the tax inner text with a $
+  subtotal.innerText = ('$' + fixedSubNum);
+
+  //Add price to tax
+  //remove $
+  var sliceTax = tax.innerText.slice(1);
+  //make sliceTax a number
+  var taxNum = parseFloat(sliceTax);
+  //add the tax for one Royale to the current tax
+  taxNum += 0.43;
+  //make sure that it only returns two decimals
+  var fixedTaxNum = taxNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  tax.innerText = '$' + fixedTaxNum;
+
+  //Add price to total
+  //remove $
+  var sliceTotal = total.innerText.slice(1);
+  //make sliceTotal a number
+  var totalNum = parseFloat(sliceTotal);
+  //add the subtotal to the tax to the previous total
+  totalNum += 15.42;
+  //make sure that it only returns two decimals
+  var fixedTotalNum = totalNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  total.innerText = '$' + fixedTotalNum;
 });
 
 // EVENT LISTENER FOR ICE CREAM
 var orderIceCream = document.querySelector('#iceCream');
 
 orderIceCream.addEventListener ('click', function () {
+  event.preventDefault();
+  // Add row with data to table
   var createTR = document.createElement('tr')
   var tdProduct = document.createElement('td');
   tdProduct.innerText = 'Ice Cream Biscut';
@@ -74,7 +197,63 @@ orderIceCream.addEventListener ('click', function () {
   createTR.appendChild(tdPrice);
   var table = document.querySelector('#table');
   table.appendChild(createTR);
+
+  // Add price to subtotal
+  //pricePattern says get the digits and decimals for whatever this is applyed to. the g means global
+  var pricePattern = /([\d\.]+)/g;
+  //remove $ using the regEx
+  var matchesSubtotal = pricePattern.exec(subtotal.innerText);
+  var subtotalNum = parseFloat(matchesSubtotal[1]);
+  //add the shown price for one Royale to the current subtotal
+  subtotalNum += 7.99;
+  //make sure that it only returns two decimals
+  var fixedSubNum = subtotalNum.toFixed(2)
+  //add the fixedTaxNum to the tax inner text with a $
+  subtotal.innerText = ('$' + fixedSubNum);
+
+  //Add price to tax
+  //remove $
+  var sliceTax = tax.innerText.slice(1);
+  //make sliceTax a number
+  var taxNum = parseFloat(sliceTax);
+  //add the tax for one Royale to the current tax
+  taxNum += 0.23;
+  //make sure that it only returns two decimals
+  var fixedTaxNum = taxNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  tax.innerText = '$' + fixedTaxNum;
+
+  //Add price to total
+  //remove $
+  var sliceTotal = total.innerText.slice(1);
+  //make sliceTotal a number
+  var totalNum = parseFloat(sliceTotal);
+  //add the subtotal to the tax to the previous total
+  totalNum += 8.22;
+  //make sure that it only returns two decimals
+  var fixedTotalNum = totalNum.toFixed(2);
+  //add the fixedTaxNum to the tax inner text with a $
+  total.innerText = '$' + fixedTotalNum;
 });
 
 // ORDER BUTTON EVENT LISTENER
 var placeOrder = document.querySelector('#placeOrder');
+
+var nameInput = document.querySelector('#name');
+var phoneInput = document.querySelector('#phone_number');
+var addressInput = document.querySelector('#address')
+
+placeOrder.addEventListener('click', function () {
+  if (total.innerText === '$0.00') {
+    Materialize.toast('Cannot place an empty order.', 4000, 'pink')
+  }
+  if (nameInput.value === '') {
+    Materialize.toast('Please enter your name.', 4000, 'pink')
+  }
+  if (phoneInput.value === '') {
+    Materialize.toast('Please enter your phone number.', 4000, 'pink')
+  }
+  if (addressInput.value === '') {
+    Materialize.toast('Please enter your address.', 4000, 'pink')
+  }
+})
